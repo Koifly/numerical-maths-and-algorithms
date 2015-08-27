2b2cc7a629e59cfafd401cd731cf7d20038bbef9
rayen <rayen.gonzalez@gmail.com>
2015-08-27 10:00:43 +0100

    Final insertionsort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,76 +1,59 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
 
 using namespace std;
 
-
-//swap swaps two integers
 int swap (int& n, int& m) {
-    int efmrl = n;
+  if (n > m) {
+    int temp = n;
     n = m;
-    m = efmrl;
-    return n, m;
+    m = temp;
+  }else {
+    return 0;
+  }
+  return 0;
 }
 
 
-//compare says if one integer is larger than another
-bool compare (int n, int m) {
-    return (n > m);
+//compares and swaps any element with all the ones before it
+vector <int> dotheThing (int a, vector <int>& v) {
+  for (a; a >= 1; a--) {
+    int i = a - 1;
+    swap (v[i], v[a]);
+  }
+  return v;
 }
 
 
-//creates a vector
-vector <int> createVector (int n) {
-    vector <int> numbers (n);
-    return numbers;
-}
-
-
-//compares one with all the ones before and swaps if necessary
-vector <int> singlePass (int n, vector <int>& efmrl) {
-    for (int i = 0; i < n; i++) {
-
-       if(compare (efmrl[i], efmrl[n])) {
-
-           swap (efmrl[i], efmrl[n]);
-       }
-       for (int i = 0; i < efmrl.size(); i++) {
-        cout << efmrl[i] << ", ";
-    }
-    cout << endl;
-    }
-    return efmrl;
-}
-
-
-
-vector <int> insertionSort (vector <int>& efmrl) {
-    for (int i = 1; i < efmrl.size(); i++) {
-
-        singlePass(efmrl[i], efmrl);
-    }
-    return efmrl;
+vector <int> insertionSort(vector <int>& v) {
+  for (int i = 1; i < v.size(); i++){
+    dotheThing (i, v);
+  }
+  return v;
 }
 
 
 int main() {
-    int a;
+  int a;
 
-    cout << "Tell me how many numbers you intend to give me." << endl;
-    cin >> a;
+  cout << "Tell me how many numbers you intend to give me." << endl;
+  cin >> a;
 
-    vector <int> values = createVector (a);
+  vector <int> numbers (a);
 
-    cout << "Good, now give me the numbers that you want me to sort. They have to be integers." << endl;
-    for (int i = 0; i < values.size(); i++) {
-        cin >> values[i];
-    }
+  cout << "Good, now give me the numbers that you want me to sort. They have to be integers." << endl;
 
-    insertionSort(values);
-    for (int i = 0; i < values.size(); i++) {
-        cout << values[i] << ", ";
-    }
-    cout << endl;
+  for (int i = 0; i < a; i++) {
+    cin >> numbers[i];
+  }
 
+  insertionSort (numbers);
+
+
+  for (int j = 0; j < numbers.size(); j++) {
+    cout << numbers[j] << ", ";
+  }
+
+
+  return 0;
 }
