40b23d512406220e21aa7b380bc7f37666985672
Rayen Gonzalez <rayen.gonzalez@gmail.com>
2016-03-11 13:04:25 +0000

    Tidy up

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,63 +1,76 @@
+// 22-09-2015
+//An application of the bubblesort algorithm
+
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 
-
 struct Sequence {
-    int a, b, c, d, e;
+  
+  int a, b, c, d, e;
 };
 
 
-int compare (int& x, int& y) {        //this compares and swaps
-    if (x > y) {
-        int perm = x;
-        x = y;
-        y = perm;
-    }else {
-        return 0;
-    }
+int compare (int& x, int& y) {
+  
+  if (x > y) {
+    int perm = x;
+    x = y;
+    y = perm;
+  }else {
+
+    return 0;
+  }
+  
 return 0;
 }
 
 
-void express (Sequence input) {       //this outputs the new sequence
-    cout << input.a << ", ";
-    cout << input.b << ", ";
-    cout << input.c << ", ";
-    cout << input.d << ", ";
-    cout << input.e << endl;
+void express (Sequence input) {
+  
+  cout << input.a << ", ";
+  cout << input.b << ", ";
+  cout << input.c << ", ";
+  cout << input.d << ", ";
+  cout << input.e << endl;
 }
 
 
 //now for the iterative swaps
 Sequence pass (Sequence& input){
-   if (input.a <= input.b && input.b <= input.c && input.c <= input.d && input.d <= input.e) {
-       return input;
-   }else {
-       compare (input.a , input.b);
-       compare (input.b , input.c);
-       compare (input.c , input.d);
-       compare (input.d , input.e);
+  
+  if (input.a <= input.b && input.b <= input.c && input.c <= input.d && input.d <= input.e) {
+    
+    return input;
+  }else {
+    compare (input.a , input.b);
+    compare (input.b , input.c);
+    compare (input.c , input.d);
+    compare (input.d , input.e);
 
-       express(input);
-       pass (input);
-   }
-   return input;
+    express(input);
+    pass (input);
+  }
+  
+  return input;
 }
 
 
 int main() {
-    Sequence numbers;
+    
+  Sequence numbers;
+  cout << "Give me five integers, hitting enter after each one." << flush;
+  cin >> numbers.a;
+  cin >> numbers.b;
+  cin >> numbers.c;
+  cin >> numbers.d;
+  cin >> numbers.e;
 
-    cout << "Give me five integers, hitting enter after each one." << flush;
-    cin >> numbers.a;
-    cin >> numbers.b;
-    cin >> numbers.c;
-    cin >> numbers.d;
-    cin >> numbers.e;
+  pass (numbers);
 
-    pass (numbers);
+  express(numbers);
 
-    express(numbers);
+  return 0;
 }
